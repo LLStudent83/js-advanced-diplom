a1ab77c36700437f7850ec2f3df9a0b3819ba0b9
Dmitry Prigulnov <prigulnov@mail.ru>
2021-08-30 17:00:37 +0500

    Написал characterGenerator, generateTeam

--- src/js/Team.js
+++ src/js/Team.js
@@ -4,19 +4,80 @@ import Character from './Character';
 
 export default class Team {
   constructor() {
-    this.arrNamesChar = ['swordsman', 'bowman', 'magician', 'daemon', 'undead', 'vampire'];
-  }
-
-  static getObjChar() {
-    class Swordsman extends Character {
-      constructor(level, type = 'generic') {
-        super(level, type = 'generic');
-      }
-    }
-    console.log(new Swordsman(1, 'swordsman'));
+    this.arrObjChar = [
+      class Swordsman extends Character {
+        constructor(level, type = 'generic') {
+          super(level, 'swordsman');
+          this.attack = 40;
+          this.defence = 10;
+        }
+      },
+      class Bowman extends Character {
+        constructor(level, type = 'generic') {
+          super(level, 'bowman');
+          this.attack = 25;
+          this.defence = 25;
+        }
+      },
+      class Magician extends Character {
+        constructor(level, type = 'generic') {
+          super(level, 'magician');
+          this.attack = 10;
+          this.defence = 40;
+        }
+      },
+      class Daemon extends Character {
+        constructor(level, type = 'generic') {
+          super(level, 'daemon');
+          this.attack = 10;
+          this.defence = 40;
+        }
+      },
+      class Undead extends Character {
+        constructor(level, type = 'generic') {
+          super(level, 'undead');
+          this.attack = 40;
+          this.defence = 10;
+        }
+      },
+      class Vampire extends Character {
+        constructor(level, type = 'generic') {
+          super(level, 'vampire');
+          this.attack = 25;
+          this.defence = 25;
+        }
+      },
+    ];
   }
 }
-Team.getObjChar();
+
+console.log(new Team().arrObjChar);
+// Стартовые характеристики (атака/защита)
+// Bowman - 25/25
+// Swordsman - 40/10
+// Magician - 10/40
+// Vampire - 25/25
+// Undead - 40/10
+// Daemon - 10/40
+//   static getObjChar() {
+//     class Swordsman extends Character {
+//       constructor(level, type = 'swordsman') {
+//         super(level, type = 'swordsman');
+//       }
+//     }
+//     class Bowman extends Character {
+//       constructor(level, type = 'bowman') {
+//         super(level, type = 'bowman');
+//       }
+//     }
+//     const arrObjChar = [
+//       new Swordsman(1, 'swordsman'),
+//       new Bowman(1, 'bowman'),
+//     ];
+//     // console.log(new Swordsman(1, 'swordsman'));
+//   }
+// }
+// Team.getObjChar();
 
 // new Daemon(level), где class Daemon extends Character
 
--- src/js/generators.js
+++ src/js/generators.js
@@ -1,3 +1,5 @@
+import Team from './Team';
+
 /**
  * Generates random characters
  *
@@ -6,13 +8,27 @@
  * @returns Character type children (ex. Magician, Bowman, etc)
  */
 export function* characterGenerator(allowedTypes, maxLevel) { // разрешонные типы
+  let levelChar = Math.floor(Math.random() * (maxLevel + 1));
+  levelChar = levelChar === 0 ? levelChar += 1 : levelChar;
+  const numberChar = Math.floor(Math.random() * (allowedTypes.length + 1));
 
-  // TODO: write logic here
-  // function getRandomInt(max) {
-  // return Math.floor(Math.random() * max);
-  // }
+  yield new allowedTypes[numberChar](levelChar);
 }
+
+// const chars = characterGenerator(new Team().arrObjChar, 2);
+// console.log('выводим результат гегератора', chars.next().value);
+
 
 export function generateTeam(allowedTypes, maxLevel, characterCount) { // количество персонажей
-  // TODO: write logic here
+  const arrObjCharTeam = [];
+  for (let i = 1; i <= characterCount;) {
+    const chars = characterGenerator(allowedTypes, maxLevel);
+
+    arrObjCharTeam.push(chars.next().value);
+    i += 1;
+  }
+  return arrObjCharTeam;
 }
+
+const arrObjCharTeam = generateTeam(new Team().arrObjChar, 3, 4);
+console.log('команда играков', arrObjCharTeam);
